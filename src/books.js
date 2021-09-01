781daeef68a7b08dfa6b14435485413736cd8927
Sachin Jambur <sachin.jambur@neofinancial.com>
2021-08-31 19:43:41 -0600

    finish basics

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+  {
+    id: 1,
+    author: "Bessels van der Kolk M.D.",
+    title:
+      "The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma",
+    img: "https://images-na.ssl-images-amazon.com/images/I/61NdJMwAThS._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 2,
+    author: "Brianna Wiest",
+    title: "The Mountain Is You: Transforming Self-Sabotage Into Self-Mastery",
+    img: "https://images-na.ssl-images-amazon.com/images/I/615NRk7ETOL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 3,
+    author: "Connor Boyack",
+    title: "How to Not Suck at Life: 89 Tips for Teens",
+    img: "https://images-na.ssl-images-amazon.com/images/I/61-btoaGcdL._AC_UL200_SR200,200_.jpg",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,47 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-
-// setup vars
-const books = [
-  {
-    author: "Bessels van der Kolk M.D.",
-    title:
-      "The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma",
-    img: "https://images-na.ssl-images-amazon.com/images/I/61NdJMwAThS._AC_UL200_SR200,200_.jpg",
-  },
-  {
-    author: "Brianna Wiest",
-    title: "The Mountain Is You: Transforming Self-Sabotage Into Self-Mastery",
-    img: "https://images-na.ssl-images-amazon.com/images/I/615NRk7ETOL._AC_UL200_SR200,200_.jpg",
-  },
-  {
-    author: "Connor Boyack",
-    title: "How to Not Suck at Life: 89 Tips for Teens",
-    img: "https://images-na.ssl-images-amazon.com/images/I/61-btoaGcdL._AC_UL200_SR200,200_.jpg",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
     <section className="bookList">
       {books.map((book) => {
-        return <Book book={book} />;
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, title, author } = props.book;
-  console.log(props);
-  return (
-    <article className="book">
-      <img src={img} alt="book" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
